a36bb6bb8f60a0e8f9cea1328b37844fba8b1db2
Seu Nome <seuemail@exemplo.com>
2025-09-19 12:56:43 -0300

    atualização

--- app.js
+++ app.js
@@ -3,59 +3,99 @@ import express from 'express';
 const servidor = express();
 servidor.use(express.json());
 
-servidor.get('/helloword', (req, res) =>
-    res.send('Hello World!'));
+servidor.get('/helloword', (req, res) => {
 
-servidor.get('/mensagem/boasvindas', (req,res) =>
-    res.send('Seja bem vindo ao meu site!'));
+    res.send({
+        "mensagem": 'Hello Word!!'
+    });
+})    
 
-servidor.get('/v2/mensagem/boasvindas', (req,res) =>
-    res.send('Seja bem vindo a minha API!'));
+servidor.get('/mensagem/boasvindas', (req,res) => {
+    res.send({
+        "mensagem": 'Seja bem vindo ao meu site!'
+    })
+});
 
-servidor.get('/mensagem/ocupado', (req, resp) =>
-    resp.send('Estou ocupado no momento!'));
+servidor.get('/v2/mensagem/boasvindas', (req,res) => {
+    res.send({
+     "mensagem":'Seja bem vindo a minha API!'
+    })
+});
 
-servidor.get('/mensagem/ocupado/recado', (req, resp) =>
-    resp.send('Estou ocupado no momento, por favor, deixe sua mensagem após o sinal!'));
+servidor.get('/mensagem/ocupado', (req, resp) =>{
+    resp.send({
+    "mensagem": 'Estou ocupado no momento!'
+    })
+});
+
+servidor.get('/mensagem/ocupado/recado', (req, resp) => {
+    resp.send({
+    "mensagem":'Estou ocupado no momento, por favor, deixe sua mensagem após o sinal!'
+    });      
+})
 
 servidor.get('/calculadora/soma/:n1/:n2', (req, resp) =>{
     let n1 = Number(req.params.n1);
     let n2 = Number(req.params.n2);
     let soma = n1 + n2;
-    resp.send('O resultado da soma é ' + soma);
+    resp.send({
+        entradas: {
+            numero1 : n1,
+            numero2 : n2
+        },        
+        soma : soma
+    });
 });
 
 servidor.get('/calculadora/subtracao/:n1/:n2', (req, resp) =>{
     let n1 = Number(req.params.n1);
     let n2 = Number(req.params.n2);
     let soma = n1 - n2;
-    resp.send('O resultado da subtração é ' + soma);
+    resp.send({
+        entradas: {
+            numero1 : n1,
+            numero2 : n2
+        },
+        soma : soma
+    });
 });
 
 servidor.get('/calculadora/multiplicacao/:n1/:n2', (req, resp) =>{
     let n1 = Number(req.params.n1);
     let n2 = Number(req.params.n2);
-    let soma = n1 * n2;
-    resp.send('O resultado da multipicacao é ' + soma);
+    let multiplicacao = n1 * n2;
+    resp.send({
+        entradas: {
+            numero1 : n1,
+            numero2 : n2
+        },        
+        multiplicacao : multiplicacao
+    });
 });
 
 servidor.get('/calculadora/divisao/:n1/:n2', (req, resp) =>{
     let n1 = Number(req.params.n1);
     let n2 = Number(req.params.n2);
-    let soma = n1 / n2;
-    resp.send('O resultado da divisao é ' + soma);
+    let divisao = n1 / n2;
+    resp.send({
+        divisao : divisao
+    });
 });
 
 servidor.get('/calculadora/somar2', (req, resp) =>{
     let n1 = Number(req.query.n1);
     let n2 = Number(req.query.n2);
     let soma = n1 + n2;
-    resp.send('O resultado da soma é ' + soma);
+    resp.send({
+        soma : soma
+    });
 });
 
 servidor.get('/mensagemOla', (req , resp) =>{
     let pessoa = req.query.nome ?? '';
-    resp.send("Olá " + pessoa)
+    resp.send({
+        "mensagem" : 'ola' + pessoa
+    })
 })
 
 servidor.post('/media', (req, resp) => {
@@ -65,8 +105,62 @@ servidor.post('/media', (req, resp) => {
 
     let media = (nota1 + nota2 + nota3) / 3;
 
-    resp.send('A média é ' + media);
+    resp.send({
+        media : media
+    });
 })
+
+servidor.post('/loja/pedido', (req, resp) => {
+    let total = req.body.total;
+    let parcelas = req.body.parcelas;
+    let cupom = req.query.cupom;
+    
+    if(parcelas > 1){
+        let juros = total * 0.05;
+        total += juros;
+    }
+
+    if(cupom == 'QUERO100'){
+        total -= 100;
+    }
+    
+    let valorParcela = total / parcelas;
+
+    resp.send({
+        total : total,
+        valorParcela : valorParcela
+    });
+})
+
+servidor.post('/loja/pedido/completo', (req,resp) => {
+    let parcelas = req.body.parcelas;
+    let itens = req.body.itens;
+    let cupom = req.query.cupom;
+
+    let total = 0;
+    for(let produto of itens){
+        total += produto.preco
+    }
+
+    if(parcelas > 1){
+        let juros = total * 0.05;
+        total += juros;
+    }
+
+    if (cupom == 'QUERO100'){
+        total -= 100;
+    }
+
+    let valorParcela = total / parcelas;
+
+    resp.send({
+        total : total,
+        qtdParcelas : parcelas,
+        valorParcela : valorParcela
+    });
+})
+
+
 
 servidor.listen(5001, ()=>
     console.log('Servidor rodando na porta 5001'));
